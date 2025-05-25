525391a4b471e408d2b4fd416b9d96f105955d03
bugrahanyaman <byaman20@ku.edu.tr>
2025-05-26 01:22:50 +0300

    Registration Screen Fixed

--- android-client/app/src/main/java/com/travelassistant/ui/screens/MainScreen.kt
+++ android-client/app/src/main/java/com/travelassistant/ui/screens/MainScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.travelassistant.ui.components.navigation.BottomNavigationBar
-import com.travelassistant.ui.navigation.NavGraph
+//import com.travelassistant.ui.navigation.NavGraph
 import com.travelassistant.ui.navigation.RootNavGraph
 
 @Composable
